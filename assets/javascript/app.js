efdae024059b83a478acb077b0c021c04aa44936
Rudy Sharar <rudysharar@D-10-19-20-179.dhcp4.washington.edu>
2018-09-22 10:15:26 -0700

    added bootstrap template for train submission form

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCT4HT-Y99MmSifr2vWdhyLVRcQzQDhtCA",
+    authDomain: "train-scheduler-e6e1f.firebaseapp.com",
+    databaseURL: "https://train-scheduler-e6e1f.firebaseio.com",
+    projectId: "train-scheduler-e6e1f",
+    storageBucket: "train-scheduler-e6e1f.appspot.com",
+    messagingSenderId: "961674848033"
+  };
+  firebase.initializeApp(config);
+
+
+//---------- VARIABLES ---------//
+
+
+
+
+//---------- FUNCTIONS ---------//
+
+
+
+//---------- EVENT LISTENERS ---------//
+
+
